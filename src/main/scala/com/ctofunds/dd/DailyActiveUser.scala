0580dfe650ca07d1e10154516379ab5aca79c373
Tang Rui <tangrui9105@sina.com>
2016-02-06 12:31:14 +0800

    Save to local file first

--- src/main/scala/com/ctofunds/dd/DailyActiveUser.scala
+++ src/main/scala/com/ctofunds/dd/DailyActiveUser.scala
@@ -1,18 +1,26 @@
 package com.ctofunds.dd
 
-import java.io.File
-import java.net.InetAddress
-
-import com.maxmind.geoip2.DatabaseReader
-import com.maxmind.geoip2.DatabaseReader.Builder
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object DailyActiveUser {
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("musically-dau")
     val sc = new SparkContext(sparkConf)
+    sc.hadoopConfiguration.set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
     val logFile = args(0)
+    val usCode = 6252001
+    val rrd = sc.textFile(logFile)
+
+    val context = rrd
+      .map(Line.parseLine)
+      .cache()
+    println("活跃用户数: " + context.count())
+    println("美国活跃用户数: " + context.filter(_.country == usCode).count())
+    println("非美国活跃用户数: " + context.filter(_.country != usCode).count())
+    println("iOS活跃用户数: " + context.filter(_.ios != -1).count())
+    println("Android活跃用户数: " + context.filter(_.android != usCode).count())
+
+    /*
     val sqlContext = new SQLContext(sc)
     val logs = sqlContext.read.json(logFile)
     val usCode = 6252001
@@ -29,13 +37,12 @@ object DailyActiveUser {
       .map(row => (row.getString(0), row.getLong(1)))
       .count()
     println(userCount)
-    /*
+
     val newUserCount = sqlContext
       .sql("SELECT COUNT(*) FROM dau WHERE requestPath = '/rest/v2/users/register' and responseCode = '200'")
       .map(row => row.getLong(0))
       .first()
     println(newUserCount)
-    */
     val ips = sqlContext
       .sql("SELECT requestUser, requestIp FROM dau WHERE requestPath = '/rest/v2/users/active' " +
         "and responseCode = '200' AND requestUser IS NOT NULL AND requestIp IS NOT NULL")
@@ -46,6 +53,14 @@ object DailyActiveUser {
     println(ips.count())
     println(ips.filter(_ == usCode).count())
     println(ips.filter(_ != usCode).count())
+
+    val agents = sqlContext
+      .sql("SELECT requestUser FROM dau WHERE requestPath = '/rest/v2/users/active' " +
+        "and responseCode = '200' AND requestUser IS NOT NULL AND userAgent LIKE '%iOS%'")
+      .map(row => (row.getString(0), 1))
+      .reduceByKey((x, y) => x)
+    println(agents.count())
+    */
 
     sc.stop()
   }
---
+++ src/main/scala/com/ctofunds/dd/SaveToLocal.scala
@@ -0,0 +1,36 @@
+package com.ctofunds.dd
+
+import java.net.InetAddress
+
+import com.maxmind.geoip2.DatabaseReader
+import com.maxmind.geoip2.DatabaseReader.Builder
+import org.apache.hadoop.io.compress.BZip2Codec
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SaveToLocal {
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("musically-dau")
+    val sc = new SparkContext(sparkConf)
+    val logFile = args(0)
+    val outFile = args(1)
+    val sqlContext = new SQLContext(sc)
+    val logs = sqlContext.read.json(logFile)
+    logs.registerTempTable("dau")
+
+    object Reader extends Serializable {
+      val database = getClass.getResourceAsStream("/geoip.mmdb")
+      val reader: DatabaseReader = new Builder(database).build()
+    }
+
+    sqlContext
+      .sql("SELECT requestUser, requestIp, userAgent FROM dau WHERE requestPath = '/rest/v2/users/active' " +
+        "and responseCode = '200' and requestUser IS NOT NULL")
+      .map(row => (row.getString(0), Array(Reader.reader.country(InetAddress.getByName(row.getString(1).stripSuffix(" ")))
+        .getCountry.getGeoNameId, row.getString(2).indexOf("iOS"), row.getString(2).indexOf("Android")).mkString(":")))
+      .reduceByKey((x, y) => x)
+      .saveAsTextFile(outFile, classOf[BZip2Codec])
+
+    sc.stop()
+  }
+}
